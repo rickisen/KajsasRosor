578039189ecd59a2a1d4de254d9d90860cb2657b
rickisen <rickard.spam@gmail.com>
2016-05-06 12:21:28 +0200

    Add more optimized images

---
+++ src/Templates/bloopers.php
@@ -0,0 +1,39 @@
+<section id="bloopers">
+    <h2>Aktuellt Just Nu</h2>
+
+    <figure>
+        <!-- an image tag that that links to a 1px big transparent png, and is given an background image instead. For html5 syntastic reasons -->
+        <img class="bg-image" style="background-image:url('/Images/yellowRose.jpg');" src="/Images/1px-trans.png" alt="Picture of a yellow rose"/> 
+        <figcaption>
+            <a href="/?/yellowRose"><h2> Gul Ros </h2></a> 
+            <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod </p>
+        </figcaption>
+    </figure> 
+
+    <figure>
+        <!-- an image tag that that links to a 1px big transparent png, and is given an background image instead. For html5 syntastic reasons -->
+        <img class="bg-image" style="background-image:url('/Images/redRose.jpg');" src="/Images/1px-trans.png" alt="Picture of a red rose"/> 
+        <figcaption>
+            <a href="/?/redRose"><h2> Röd Ros </h2></a> 
+            <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod </p>
+        </figcaption>
+    </figure> 
+
+    <figure>
+        <!-- an image tag that that links to a 1px big transparent png, and is given an background image instead. For html5 syntastic reasons -->
+        <img class="bg-image" style="background-image:url('/Images/pinkRose.jpg');" src="/Images/1px-trans.png" alt="Picture of a pink rose"/> 
+        <figcaption>
+            <a href="/?/pinkRose"><h2> Rosa Ros </h2></a> 
+            <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod </p>
+        </figcaption>
+    </figure> 
+
+    <figure>
+        <!-- an image tag that that links to a 1px big transparent png, and is given an background image instead. For html5 syntastic reasons -->
+        <img class="bg-image" style="background-image:url('/Images/whiteRose.jpg');" src="/Images/1px-trans.png" alt="Picture of a white rose"/> 
+        <figcaption>
+            <a href="/?/whiteRose"><h2> Vit Ros </h2></a> 
+            <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod </p>
+        </figcaption>
+    </figure> 
+</section>
