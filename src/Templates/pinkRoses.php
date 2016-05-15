ef9511be96421458a49b0f4a214e9460dfda6450
rickisen <rickard.spam@gmail.com>
2016-05-15 12:01:34 +0200

    Change Content structure, closer to example

---
+++ src/Templates/pinkRoses.php
@@ -0,0 +1,88 @@
+<div id="hero" class="bg-image" style="background-image:url('/Images/heroRed.jpg')" alt="Picture of a rose"> </div>
+
+<main>
+    <header> 
+        <span> Kajsas Rosor > Snittrosor > Rosa Rosor </span>
+    </header>
+
+    <article>
+        <header>
+            <h1>Rosa Rosor</h1>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+
+        <section>
+            <h2>Svenska Inrednings Trender</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+                vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+                no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+
+            <img src="/Images/pinkRoses.jpg" alt="Collection of pink roses"/>
+        </section>
+
+        <section>
+            <h2>Rosen Idag</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+                vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+                no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+        </section>    
+    </article>
+
+    <aside>
+        <h2>Blomster Språket</h2>
+
+        <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+
+        <section>
+            <h3>Se efter vad just din favoritblomma står för</h3>
+
+            <section>
+                <h4>En blomma</h4>
+                <p>du är mitt allt</p>
+            </section>
+
+            <section>
+                <h4>Två blommor</h4>
+                <p>du är mitt allt</p>
+            </section>
+
+            <section>
+                <h4>Tre blommor</h4>
+                <p>du är mitt allt</p>
+            </section>
+
+            <section>
+                <h4>Fyra blommor</h4>
+                <p>du är mitt allt</p>
+            </section>
+        </section>
+    </aside>
+
+    <footer> 
+        <address>
+            Kajsas Rosor, Rosvägen 16. 113 53 Stockholm | 08-422 95 00 | <span class="pink">info@kajsasrosor.se</span>
+        </address>
+    </footer>
+</main>
---
+++ src/Templates/snittRosor.php
@@ -0,0 +1,51 @@
+<div id="hero" class="bg-image" style="background-image:url('/Images/heroRed.jpg')" alt="Picture of a rose"> </div>
+
+<main>
+    <header> 
+        <span> Kajsas Rosor > Snitt Rosor </span>
+    </header>
+
+    <article>
+        <header>
+            <h1>Snitt Rosor</h1>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+
+        <?php include "bloopers.php"?>
+
+    </article>
+
+    <aside>
+        <h2>Skötselråd</h2>
+
+        <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+        </p>
+
+        <p>
+            - Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet. <br>
+            - Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>
+            - Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>
+
+        </p>
+    </aside>
+
+    <footer> 
+        <address>
+            Kajsas Rosor, Rosvägen 16. 113 53 Stockholm | 08-422 95 00 | <span class="pink">info@kajsasrosor.se</span>
+        </address>
+    </footer>
+</main>
